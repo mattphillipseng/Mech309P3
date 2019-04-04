9637488305846eff872b8a40e3d916a5536b1a5c
Matthew Phillips <matthewp1234@gmail.com>
2019-04-03 22:56:30 -0400

    initial code

---
+++ main.m
@@ -0,0 +1,17 @@
+%% MECH 309 Final Project
+
+% Modified by Matthew Phillips
+% Student ID: 260689202
+
+% Modified by Jarred Brown
+% Student ID: 260688463
+
+close all
+clear all
+clc
+
+% Format of output
+    % format short
+    format long
+
+%%
